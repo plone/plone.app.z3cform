ba1a570d0ccceb9b39c1fec9cd560d422d9d23bf
Daniel Nouri <daniel.nouri@gmail.com>
2008-07-22 23:11:07 +0000

    Catch up with rename in plone.z3cform
    svn path=/plone.app.z3cform/trunk/; revision=21808

--- plone/app/z3cform/layout.py
+++ plone/app/z3cform/layout.py
@@ -1,9 +1,9 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
-import plone.z3cform.base
+import plone.z3cform.layout
 
-class FormWrapper(plone.z3cform.base.FormWrapper):
+class FormWrapper(plone.z3cform.layout.FormWrapper):
     index = ViewPageTemplateFile('layout.pt')
 
 def wrap_form(form, __wrapper_class=FormWrapper, **kwargs):
-    return plone.z3cform.base.wrap_form(form, __wrapper_class, **kwargs)
+    return plone.z3cform.layout.wrap_form(form, __wrapper_class, **kwargs)

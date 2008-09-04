9f322f36f6fa9c8fe302897eec7c01597fdfd2be
Daniel Nouri <daniel.nouri@gmail.com>
2008-09-04 15:12:03 +0000

    Make the WYSIWYG widget work also in for non-Acquisition wrapped contexts.
    svn path=/plone.app.z3cform/trunk/; revision=22487

--- plone/app/z3cform/wysiwyg/widget.py
+++ plone/app/z3cform/wysiwyg/widget.py
@@ -1,5 +1,7 @@
+import Acquisition
 import zope.interface
 import zope.schema.interfaces
+from zope.app.component.hooks import getSite
 
 import z3c.form.interfaces
 import z3c.form.browser.textarea
@@ -17,6 +19,15 @@ class WysiwygWidget(z3c.form.browser.textarea.TextAreaWidget):
     def update(self):
         super(z3c.form.browser.textarea.TextAreaWidget, self).update()
         z3c.form.browser.widget.addFieldClass(self)
+
+        # We'll wrap context in the current site *if* it's not already
+        # wrapped.  This allows the template to acquire tools with
+        # ``context/portal_this`` if context is not wrapped already.
+        # Any attempts to satisfy the Kupu template in a less idiotic
+        # way failed:
+        if getattr(self.context, 'aq_inner', None) is None:
+            self.context = Acquisition.ImplicitAcquisitionWrapper(
+                self.context, getSite())
 
 @zope.component.adapter(zope.schema.interfaces.IField,
                         z3c.form.interfaces.IFormLayer)

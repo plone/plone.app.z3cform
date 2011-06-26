c26bef83652fcf5687ac7666ee76a8c2caf452dc
David Glick <davidglick@groundwire.org>
2011-06-26 17:43:14 +0000

    Prepare plone.app.z3cform 0.5.5.
    svn path=/plone.app.z3cform/trunk/; revision=50772

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.5.4'
+version = '0.5.5'
 
 long_description = (
     read('README.txt')

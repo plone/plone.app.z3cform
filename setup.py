7d5b32e3aba25899d334a27795a745d49cc48566
Eric Steele <EricSteele47@gmail.com>
2012-05-07 21:45:42 -0400

    Preparing release 0.5.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.5.8dev'
+version = '0.5.8'
 
 long_description = (
     read('README.txt')

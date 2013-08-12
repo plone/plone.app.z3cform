6d9bf264e38aa7b2dddd6b43f7a5bbdb21d53eee
Juan Pablo Giménez <jpg@rcom.com.ar>
2013-08-12 12:00:48 -0300

    Document ARIA methods.

--- plone/app/z3cform/aria.py
+++ plone/app/z3cform/aria.py
@@ -4,10 +4,14 @@ from Products.Five import BrowserView
 class AriaHelperView(BrowserView):
 
     def describedby(self):
-    	if self.context.field.description and self.context.mode != 'hidden':
-    		return 'formfield-%s-formHelp' % self.context.id
-    	return None
+        """Identifies the element that describes the field input.
+        """
+        if self.context.field.description and self.context.mode != 'hidden':
+        	return 'formfield-%s-formHelp' % self.context.id
+        return None
 
     def required(self):
-    	return str(self.context.required and \
-    			self.context.mode == 'input').lower()
+        """Indicates that user input is required on the element before a form may be submitted.
+        """
+        return str(self.context.required and \
+            self.context.mode == 'input').lower()
--- plone/app/z3cform/interfaces.py
+++ plone/app/z3cform/interfaces.py
@@ -1,5 +1,20 @@
+from zope.interface import Interface
 from z3c.form.interfaces import IFormLayer
+
 
 class IPloneFormLayer(IFormLayer):
     """Request layer installed via browserlayer.xml
     """
+
+
+class IAriaHelperView(Interface):
+    """Helper view to get ARIA attributes
+    """
+
+    def describedby():
+        """Identifies the element that describes the field input.
+        """
+
+    def required():
+        """Indicates that user input is required on the element before a form may be submitted.
+        """

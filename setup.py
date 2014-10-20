40fdad983db93ba8926dad0c536a313ee98d4319
esteele <eric@esteele.net>
2014-10-20 14:49:37 -0400

    Preparing release 0.7.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.7.7.dev0'
+version = '0.7.7'
 
 long_description = (
     read('README.rst')

531893b7323a7a866f8afc96ac1c103fc797f245
Godefroid Chapelle <gotcha@bubblenet.be>
2016-04-13 13:21:24 +0200

    Back to development: 0.7.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.7.8'
+version = '0.7.9.dev0'
 
 long_description = (
     read('README.rst')

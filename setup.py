d741f04aa1775004e7b98e95a504314cf7f3f7ea
Maurits van Rees <maurits@vanrees.org>
2016-08-12 18:32:14 +0200

    Back to development: 2.1.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '2.1'
+version = '2.1.1.dev0'
 
 long_description = (
     read('README.rst') +

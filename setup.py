0d3c0808e545df55b9f33eab8ea5dda81b4f79b6
Maurits van Rees <maurits@vanrees.org>
2016-09-16 18:49:15 +0200

    Removed Plone 5.0 classifier from setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
     long_description=long_description,
     classifiers=[
         "Framework :: Plone",
-        "Framework :: Plone :: 5.0",
         "Framework :: Plone :: 5.1",
         "Framework :: Zope2",
         "Programming Language :: Python",

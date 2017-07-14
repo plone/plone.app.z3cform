00ebba20e245d3df68c6d255f918ccf5ec9e5682
Johannes Raggam <thetetet@gmail.com>
2017-07-14 12:34:16 +0200

    PLIP 2150: Change the input type for date fields to date and for datetime fields to datetime-local.

--- plone/app/z3cform/widget.py
+++ plone/app/z3cform/widget.py
@@ -156,6 +156,7 @@ class DateWidget(BaseWidget, HTMLInputWidget):
         args['name'] = self.name
         args['value'] = (self.request.get(self.name,
                                           self.value) or u'').strip()
+        args['type'] = 'date'
 
         args.setdefault('pattern_options', {})
         if self.field.required:
@@ -229,6 +230,8 @@ class DatetimeWidget(DateWidget, HTMLInputWidget):
 
         if args['value'] and len(args['value'].split(' ')) == 1:
             args['value'] += ' 00:00'
+
+        args['type'] = 'datetime-local'
 
         args.setdefault('pattern_options', {})
         if 'time' in args['pattern_options']:

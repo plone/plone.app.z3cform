cbddc47979ccd00fe062f8554d2ef9de92c7f873
Johannes Raggam <thetetet@gmail.com>
2017-09-19 16:54:52 +0200

    restructure for better readability

--- plone/app/z3cform/widget.py
+++ plone/app/z3cform/widget.py
@@ -149,8 +149,8 @@ class DateWidget(BaseWidget, HTMLInputWidget):
         """
         args = super(DateWidget, self)._base_args()
         args['name'] = self.name
-        args['value'] = (self.request.get(self.name,
-                                          self.value) or u'').strip()
+        val = self.request.get(self.name, self.value) or u''
+        args['value'] = val.strip()
         args['type'] = 'date'
 
         args.setdefault('pattern_options', {})
@@ -159,7 +159,8 @@ class DateWidget(BaseWidget, HTMLInputWidget):
             args['pattern_options']['clear'] = False
         args['pattern_options'] = dict_merge(
             get_date_options(self.request),
-            args['pattern_options'])
+            args['pattern_options']
+        )
 
         return args
 
@@ -871,3 +872,4 @@ class SingleCheckBoxBoolWidget(SingleCheckBoxWidget):
 def SingleCheckBoxBoolFieldWidget(field, request):
     """IFieldWidget factory for CheckBoxWidget."""
     return FieldWidget(field, SingleCheckBoxBoolWidget(request))
+

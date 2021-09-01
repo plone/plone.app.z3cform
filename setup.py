37fd06f60fa865e6999fc7d5fdd1a0a0f290bfce
Maurits van Rees <maurits@vanrees.org>
2021-09-01 17:58:39 +0200

    Add Py 3.9 classifier.
    [ci skip]

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords='zope plone form widget template',
     author='Plone Foundation',

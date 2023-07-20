b65fc7a9bb2aec99875c00754f671192ffba96b1
Peter Mathis <peter.mathis@kombinat.at>
2023-07-20 15:43:39 +0200

    Refactor base pattern widget

--- plone/app/z3cform/widgets/base.py
+++ plone/app/z3cform/widgets/base.py
@@ -1,6 +1,9 @@
 from Acquisition import aq_base
 from plone.app.z3cform.utils import call_callables
+from z3c.form.browser import widget
 from z3c.form.widget import Widget
+
+import json
 
 
 class PatternNotImplemented(Exception):
@@ -60,3 +63,60 @@ class BaseWidget(Widget):
 
     def is_subform_widget(self):
         return getattr(aq_base(self.form), "parentForm", None) is not None
+
+
+class PatternFormElement(widget.HTMLFormElement):
+    """New implementation of pattern widget with z3c.form extendable attributes"""
+
+    _klass_prefix = "pat-"
+
+    pattern = None
+    pattern_options = {}
+
+    def get_pattern_options(self):
+        """override this factory to inject the pattern options as
+        "data-<self._klass_prefix><self.pattern>" attribute
+        """
+        return self.pattern_options
+
+    @property
+    def attributes(self):
+        """add "required" attribute"""
+
+        attributes = super().attributes
+
+        if self.field.required:
+            attributes["required"] = "required"
+
+        if self.pattern:
+            pat_options = call_callables(
+                self.get_pattern_options().copy(),
+                self.context,
+            )
+            attributes[f"data-{self._klass_prefix}{self.pattern}"] = json.dumps(
+                pat_options
+            )
+
+        return attributes
+
+    def update(self):
+        super().update()
+        if self.pattern:
+            self.addClass(f"{self._klass_prefix}{self.pattern}")
+        widget.addFieldClass(self)
+
+
+class HTMLInputWidget(PatternFormElement, widget.HTMLInputWidget):
+    """InputWidget with pattern options"""
+
+
+class HTMLTextInputWidget(PatternFormElement, widget.HTMLTextInputWidget):
+    """TextInputWidget with pattern options"""
+
+
+class HTMLTextAreaWidget(PatternFormElement, widget.HTMLTextAreaWidget):
+    """TextAreaWidget with pattern options"""
+
+
+class HTMLSelectWidget(PatternFormElement, widget.HTMLSelectWidget):
+    """SelectWidget with pattern options"""

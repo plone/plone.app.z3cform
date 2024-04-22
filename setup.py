07375b59f3343e90b9ef241fbdb44a471dcf1d8a
Maurits van Rees <maurits@vanrees.org>
2024-04-22 12:04:48 +0200

    Bumped version for feature release.
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "4.5.1.dev0"
+version = "4.6.0.dev0"
 
 long_description = (
     f"{Path('README.rst').read_text()}\n"
